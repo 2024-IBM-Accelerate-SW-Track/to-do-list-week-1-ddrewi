1dd7b975df16ff5c44aa43f8de631f9f10f5f84b
ddrewi <84168601+ddrewi@users.noreply.github.com>
2024-06-05 17:00:01 -0700

    Finished about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,11 +1,31 @@
 import React, { Component } from 'react';
+import "./About.css";
+import pic from "../assets/profile_pic.jpg";
   
 export default class About extends Component {
   render() {
     return (
       <div>
-        <p>Design your About me page </p> 
-      </div>
+        <div className="split left">
+          <div className="centered">
+            <img className="profile_image" src={ pic } alt="Profile Pic"></img>
+          </div>
+        </div>
+        <div className="split right">
+          <div className="centered">
+            <div className="name_title">Andrew Di</div>
+              <div className="brief_description">
+                <p>
+                  CS major at the University of Illinois Urbana-Champaign.<br />
+                  Broadly interested in "making things go fast".<br />
+                  Loves to take walks and look at trees.<br />
+                  Spends too much time playing Sudoku, wondering what to eat, and dreading existential crises.<br />
+                  Excited to meet new people => Feel free to reach out!
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
     )
   }
 }
